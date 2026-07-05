39e2279e838a5d110e088be48cf8948291144917
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2026-07-04 23:20:17 -0700

    dk0 2.4.2.244

--- dk0.cmd
+++ dk0.cmd
@@ -47,8 +47,8 @@ REM
 REM   Empty value if the architecture is not supported.
 REM   In particular, use empty instead of 9491d4737000e80bcbdd7a39e9dc13c2178ff865beff7d800d6159bfc395e8fa which is checksum for HTTP 404 error.
 REM -------------------------------------
-SET DK_VER=2.4.2.233
-SET DK_CKSUM_WINDOWS_X86_64=0cc08b8400a5fb1c7f29b510fe4bdc65f7025744619697ce515c7705f0529774
+SET DK_VER=2.4.2.244
+SET DK_CKSUM_WINDOWS_X86_64=7ba377096776efa7afd2e06f5cec3a8e030289b41fe560dc552572e18fd6c4fb
 SET DK_CKSUM_WINDOWS_X86=
 
 REM --------- Quiet Detection ---------
